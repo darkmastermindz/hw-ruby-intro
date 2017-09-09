1e7527758b8a21ba3625e3b6bebf60fb4b72a6bf
hwei3 <hwei3@uncc.edu>
2017-09-09 17:18:48 -0400

    Finished Assignment

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,89 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each{ |a| sum+=a }
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.sort.reverse.take(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  c = 0
+  li = arr.length -1
+
+  if arr.size < 2
+    return false
+  end
+
+  arr_copy = arr.sort
+  
+  loop do		
+    break if c >= li
+    
+    if arr_copy[c] + arr_copy[li] == n
+       return true
+    elseif arr_copy[l] + arr_copy[li] < n
+       c +=1
+    else
+      r -= 1
+    end
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.length == 0 return false end
+  "bcdfghjklmnpqrstvwxyz".include? s[0].downcase
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s != "0" && (s.length < 3 || nil == (/^[01]+$/ =~ s) || s.reverse[0,2] != "00")
+    return false
+  else
+    return true
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+  def initialize(isbn, price)
+    self.isbn = isbn
+    self.price = price
+  end
+  
+  def isbn
+    @ISBN
+  end
+  
+  def isbn=(new_ISBN)
+    if new_ISBN.length == 0
+      raise ArgumentError, 'ISBN is not valid'
+    end
+    
+    @ISBN = new_ISBN
+  end
+  
+  def price
+    @price
+  end
+  
+  def price=(new_price)
+    if new_price <= 0
+      raise ArgumentError, 'Price must be greater than 0'
+    end  
+    @price = new_price
+  end
+  
+  def price_as_string
+    "$%.2f" % @price
+  end
 end
